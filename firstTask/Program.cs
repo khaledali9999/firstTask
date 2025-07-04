261026e31dfe2ad369fb6be5f662c1e21cb7a833
khaledali9999 <elsaadanykhaled5@gmail.com>
2025-07-04 18:41:54 +0300

    Program.cs

--- firstTask/Program.cs
+++ firstTask/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("How many small carpets do you want to clean?");
             int minValue = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("How many small carpets do you want to clean?");
+            Console.WriteLine("How many large carpets do you want to clean?");
             int maxValue = Convert.ToInt32(Console.ReadLine());
             int costValue = (maxValue * 35) + (minValue * 25);
             double tax = (costValue * 0.06);
