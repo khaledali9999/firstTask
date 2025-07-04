76ec2f5a4b0748167b4b541cb45e256f7dd48479
Mega Store <Mega Store@DESKTOP-C4M8BPS>
2025-07-04 18:39:04 +0300

    Add project files.

---
+++ firstTask/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace firstTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many small carpets do you want to clean?");
+            int minValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many small carpets do you want to clean?");
+            int maxValue = Convert.ToInt32(Console.ReadLine());
+            int costValue = (maxValue * 35) + (minValue * 25);
+            double tax = (costValue * 0.06);
+            double total = costValue + tax;
+            Console.WriteLine( "ok your final amount is: " + total);
+        }
+    }
+}
